e6715fca645a2b0facc909897ab20da9850298dd
Ying Zhang <yingz@ebay.com>
2018-03-17 11:36:36 -0700

    passport JWT token

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var config = require('./config');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -12,8 +16,7 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
-var url = 'mongodb://localhost:27017/conFusion';
-mongoose.connect(url);
+mongoose.connect(config.mongoUrl);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
@@ -35,6 +38,14 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(cookieParser());
+
+// passport config
+var User = require('./models/user');
+app.use(passport.initialize());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
@@ -50,15 +61,27 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            error: err
+        });
+    });
+}
 
-    // render the error page
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error');
+    res.json({
+        message: err.message,
+        error: {}
+    });
 });
 
 module.exports = app;
