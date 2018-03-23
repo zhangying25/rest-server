f65b2ff07b37c3f487c21fc4b69a84fd20140b1e
Ying Zhang <yingz@ebay.com>
2018-03-22 17:39:44 -0700

    oauth

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '1637212686325883',
+        clientSecret: '91e349d508bcca2bb49f19ce7ed89e30',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
